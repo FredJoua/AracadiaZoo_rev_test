28191939a257ab31042be8bcc20a38f9756bc0e9
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-11-28 18:49:54 +0100

    header test

--- pageStatiques/header.php
+++ pageStatiques/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <!-- Bootstrap CSS -->
+    <!-- test Bootstrap CSS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link
