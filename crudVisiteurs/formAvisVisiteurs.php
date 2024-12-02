7778555c267c573b9d239c59755c77d797c303ba
FredJoua <165488829+FredJoua@users.noreply.github.com>
2024-12-02 22:07:31 +0100

    commit branch_crudVisiteurs

--- crudVisiteurs/formAvisVisiteurs.php
+++ crudVisiteurs/formAvisVisiteurs.php
@@ -1,6 +1,5 @@
 <?php
 include_once "../dbconn.php"; 
-include_once "../header.php"; 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Récupérer les données du formulaire
@@ -48,20 +47,19 @@ $cnx = null;
     <title>Contactez-nous</title>
 
     <!-- Bootstrap CSS-->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="styleFooter.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <style>
+        body {
+            padding-top: 75px; 
+        }
+        .mb-5 {
+            margin-bottom: 50px; 
+        }
+    </style>
 </head>
 
 <body>
+    <?php include_once "../pagesFront/header.php"; ?>
 
 <section class="form my-4 mx-5">
     <div class="container">
@@ -98,6 +96,7 @@ $cnx = null;
     </div>
 </section>
 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
-<?php include "../footer.php" ?>
+    <?php include "../pagesFront/footer.php"; ?>
 </html>
--- crudVisiteurs/tableListingCommentsVisiteurs.php
+++ crudVisiteurs/tableListingCommentsVisiteurs.php
@@ -57,13 +57,19 @@
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.css">
     <!-- Font Awesome CDN link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/styleHeaderLogout.css">
+
+    <style>
+        body {
+            padding-top: 75px; /* Ajustez cette valeur selon besoins */
+        }
+
+    </style>
+
 </head>
+
 <body>
-    <header>
-        <?php
-        include_once '../headerlogout.php';
-        ?>
-    </header>
+    <?php include_once "../pagesFront/headerLogout.php"; ?>
 
     <section class="container my-5">
         <h3>Liste des Commentaires</h3>
@@ -107,12 +113,12 @@
     </section>
 
     <!-- Bootstrap js link --> 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
     <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/locale/bootstrap-table-fr-FR.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
 </body>
-    <?php
-    include_once '../footer.php';
-    ?>
+    <?php include_once '../pagesFront/footer.php'; ?>
 </html>
